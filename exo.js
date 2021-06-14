194ae3a221f6f471278b2f984c705e8fc1e3943b
juju-v21 <85849575+juju-v21@users.noreply.github.com>
2021-06-14 22:35:44 +0200

    Add files via upload

---
+++ exo.js
@@ -0,0 +1,234 @@
+/*                             correction exo 1 
+
+let pepito = "gateau";
+let orange = "fruit";
+
+
+[orange,pepito] = [pepito,orange];
+
+*/
+
+
+/* autre solutions de l'exercice 1 ? 
+
+/*let a = "a" + 9;
+let b = "b" + 6;
+
+[a,b] = [b,a]
+document.write(a, b);
+console.log(a,b);*/
+
+
+
+/*                              correction exo 2                     
+var nom = "toto "
+var age = '30'
+var homme = " true"
+
+document.write(nom+age+homme)
+*/
+
+/*                               correction exo 3.1
+var a = prompt("entrer nombre")
+console.log(Math.pow(a,2))
+*/
+
+
+/*                               correction exo 3.2
+var a =parseInt(prompt("enter un nombre: "))
+var b =parseInt(prompt("entrer un nombre: "))
+
+function calc(a, b){
+   var result = a + b
+   return result;
+}
+console.log(calc(a, b))
+*/
+
+/*                              
+var a=1
+
+a +=1
+
+console.log(a)
+*/
+
+/*                               correction exo 3.3
+var a=3
+
+a *=60
+
+console.log(a)
+*/
+
+/*                               correction exo 4
+var a=1
+
+a ++
+
+console.log(a)
+*/
+
+/*                               correction exo 5
+var a=2
+var b=3
+var c=a*b
+
+c /=2
+ 
+console.log(c)
+*/
+
+/*                                correction exo 6
+
+var a ="blabla"
+b = a.split('').reverse('').join('')
+
+console.log(b)
+*/
+
+
+/*                                correction exo 7
+var a =("1", "2", "3")
+Math.max(a)
+
+console.log(a)
+*/
+
+/*                                correction exo 8
+var a = ["a", "b", "c"]
+
+var first = a[0]
+
+console.log(first)
+*/
+
+/*                               correction exo 9
+var a = document.getElementById("biscuit").innerHTML =
+"l'url est: " +  window.location.href
+*/
+
+/*                               correction exo 10
+var a=2
+var b=2
+
+c =a % b
+pour trouver le reste c'est le remainder en js 
+
+console.log(c)
+*/
+
+/*                                correction exo 11
+var a=2
+var b=1
+var c = a+b
+
+var result 
+    if (c < 100){
+        result ='true';
+    }
+    else {
+        result ='fasle'
+    }
+
+console.log(result)
+*/
+
+
+/*                                correction exo 12
+var heure =3600
+var min =60
+
+var a =3
+
+a *=heure
+
+console.log(a)
+*/
+
+/*                                correction exo 13
+var a = prompt("enter un nombre: ")
+var b 
+if (b <= 0) {
+    b ='true'
+} else {
+    b ='false'
+}
+
+console.log(b)
+*/
+
+/*                                correction exo 14
+var a =0
+var b =0
+
+var equal =a+b
+if (a == b){
+    equal ='true'
+}
+else {
+    equal ='false'
+}
+
+console.log(equal)
+*/
+
+/*                                  correction exo 15                 
+var a = prompt("entrer un nombre")
+if ( a %5 == 0){
+    a ='true'
+}
+else {
+    a ='fasle'
+}
+console.log(a)
+*/
+
+
+/*                                   correction exo 16
+var heure =3600
+var min =60
+var a =prompt("entrer un nombre")*heure
+var b =prompt("entrer un nombre")*min
+var c =(a+b)
+
+console.log(c)
+*/
+
+
+/*                              correction exo 17
+var a =["a", "b", "c"]
+
+["a", "b", "c"] = ["c", "b", "a"]
+
+console.log(a)
+*/
+
+/*                             correction exo 18
+var a =["1", "2", "3", "4"]
+
+var LastElement = [a.length-0]
+
+console.log(LastElement)
+*/
+
+/*                             correction exo 19
+var a =prompt("enter nombre: ")
+var b =prompt("enter un autre nombre: ")
+
+if (a === b){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+*/
+
+/*                             correction exo 20
+var a =prompt("enter un nombre")
+if (a === ""){
+    console.log(true)
+} else {
+    console.log(false)
+}
+*/
